b1a483478107d3500bffd4e84ba5d57ac01b867f
eminthwu <andrew.liu@advantech.com.tw>
2017-01-08 17:07:29 +0800

    Add Parameter class

---
+++ TDDPartitionThenSum/Parameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDPartitionThenSum
+{
+   public class Parameter
+    {
+        public Columns ParticularColumn { get; set; }
+
+        public int Rows { get; set; }
+    }
+
+    public enum Columns
+    {
+        ID,Cost,Revenue,SellPrice
+    }
+}
