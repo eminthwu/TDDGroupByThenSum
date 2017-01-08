59de1b52d19caf314b0f1085f73396fdbe2f0819
eminthwu <andrew.liu@advantech.com.tw>
2017-01-08 16:43:42 +0800

    ChangeName

--- TDDPartitionThenSum/TDDPartitionThenSum.cs
+++ TDDPartitionThenSum/TDDPartitionThenSum.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TDDGroupByThenSum
+namespace TDDPartitionThenSum
 {
-    public class Class1
+    public class TDDPartitionThenSum
     {
     }
 }
