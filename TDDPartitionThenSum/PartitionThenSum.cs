f06d2c1c0a64fd6a69d75b4e11a827b7202ac525
eminthwu <andrew.liu@advantech.com.tw>
2017-01-08 17:07:06 +0800

    Rename Class

--- TDDPartitionThenSum/PartitionThenSum.cs
+++ TDDPartitionThenSum/PartitionThenSum.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TDDPartitionThenSum
 {
-    public class TDDPartitionThenSum
+    public class PartitionThenSum
     {
     }
 }
---
+++ TDDPartitionThenSumTest/PartitionThenSumTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using TDDPartitionThenSum;
+using System.Collections.Generic;
+
+namespace TDDGroupByThenSumTest
+{
+    [TestClass]
+    public class TDDPartitionThenSumTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var target = new PartitionThenSum();
+            var parm = new Parameter()
+            {
+                Rows = 3,
+                ParticularColumn = Columns.Cost
+            };
+
+            var expected = new List<int>() { 6, 151, 24, 21 };
+
+            //Act
+            List<int> result = target.GetSumResult(parm);
+            //Assert
+        }
+    }
+}
